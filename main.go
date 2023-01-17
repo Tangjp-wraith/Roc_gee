00b2f560c0ddb4e70eed4916d6d9dd7091190f4f
Tangjp-wraith <3081129260@qq.com>
2023-01-17 14:40:30 +0800

    Implement Panic Recover

--- main.go
+++ main.go
@@ -1,52 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"gee"
-	"html/template"
 	"net/http"
-	"time"
 )
 
-type student struct {
-	Name string
-	Age  int8
-}
-
-func FormatAsDate(t time.Time) string {
-	year, month, day := t.Date()
-	return fmt.Sprintf("%d-%02d-%02d", year, month, day)
-}
-
 func main() {
-	r := gee.New()
-	r.Use(gee.Logger()) // global middleware
-
-	r.SetFuncMap(template.FuncMap{
-		"FormatAsDate": FormatAsDate,
-	})
-	r.LoadHTMLGlob("templates/*")
-	r.Static("/assets", "./static")
-
+	r := gee.Default()
 	r.GET("/", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "css.tmpl", gee.H{})
+		c.String(http.StatusOK, "Hello Geektutu\n")
 	})
-
-	stu1 := &student{Name: "Geektutu", Age: 20}
-	stu2 := &student{Name: "tang", Age: 21}
-	r.GET("/stu", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "arr.tmpl", gee.H{
-			"title":  "gee",
-			"stuArr": [2]*student{stu1, stu2},
-		})
+	// index out of range for testing Recovery()
+	r.GET("/panic", func(c *gee.Context) {
+		names := []string{"geektutu"}
+		c.String(http.StatusOK, names[100])
 	})
-
-	r.GET("/date", func(c *gee.Context) {
-		c.HTML(http.StatusOK, "custom_func.tmpl", gee.H{
-			"title": "gee",
-			"now":   time.Date(2019, 8, 17, 0, 0, 0, 0, time.UTC),
-		})
-	})
-
 	r.Run(":9999")
 }
